b2af695215b0eff8dbfeabbc6fdc77d731d8b704
Orewa0Luffy <ichigobotam1@gmail.com>
2025-05-29 09:56:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,7 +68,7 @@ APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 # MongoDB Configuration
 # ============================
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://filterunionbot:animeotaku109@cluster0.ljgsowr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://tyliqedy:animeotaku109@cluster0.6s6oik5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://lorele:animeotaku109@cluster0.iwlghqv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "FilterBot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjcollection')
 
